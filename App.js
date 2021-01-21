d9df556036f91f546f10b6bfe174a5bd29d4e807
Murad07 <murad.pi22@gmail.com>
2021-01-21 10:24:35 +0600

    My account page design

--- App.js
+++ App.js
@@ -1,61 +1,69 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import * as React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
 
 // Routing - go one screen or page to another
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 // Routing
 
-import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
-import SearchPost from './components/SearchPost';
-import WantTo from './components/WantTo';
-import Login from './components/Login';
-import SignInUp from './components/SignInUp';
-import MyAccount from './components/MyAccount';
-import Brand from './components/Brand';
+import SearchPost from "./components/SearchPost";
+import WantTo from "./components/WantTo";
+import Login from "./components/Login";
+import SignInUp from "./components/SignInUp";
+import MyAccount from "./components/MyAccount";
+import Brand from "./components/Brand";
+import AboutMe from "./components/AboutMe";
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
       <Button
-        title='Go to Details'
-        onPress={() => navigation.navigate('Details')}
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
       />
       <Button
-        title='Go to Brand Name'
-        onPress={() => navigation.navigate('Brand')}
+        title="Go to Brand Name"
+        onPress={() => navigation.navigate("Brand")}
       />
       <Button
-        title='Search or Post'
-        onPress={() => navigation.navigate('Search or Post')}
+        title="Search or Post"
+        onPress={() => navigation.navigate("Search or Post")}
       />
       <Button
-        title='I want to'
-        onPress={() => navigation.navigate('I want to')}
+        title="I want to"
+        onPress={() => navigation.navigate("I want to")}
       />
 
-<Button
-        title='Login'
-        onPress={() => navigation.navigate('Login')}
+      <Button
+        title="About Me"
+        onPress={() => navigation.navigate("About Me")}
       />
+
+      <Button
+        title="My Account"
+        onPress={() => navigation.navigate("My Account")}
+      />
+
+      <Button title="Login" onPress={() => navigation.navigate("Login")} />
     </View>
   );
 }
 
 function DetailsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen 1</Text>
       <Button
-        title='Go to Details... again'
-        onPress={() => navigation.push('Details')}
+        title="Go to Details... again"
+        onPress={() => navigation.push("Details")}
       />
 
-      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
-      <Button title='Go back' onPress={() => navigation.goBack()} />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -66,7 +74,7 @@ function App() {
   // Loading custom fonts from assets folder
   // we can add more fonts here
   let [fontsLoaded] = useFonts({
-    'Lato-Black': require('./assets/fonts/Lato-Black.ttf'),
+    "Lato-Black": require("./assets/fonts/Lato-Black.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -75,15 +83,25 @@ function App() {
     return (
       // Routing - go one screen or page to another
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Brand'>
-          <Stack.Screen name='Brand' component={Brand} />
-          <Stack.Screen name='Home' component={HomeScreen} />
-          <Stack.Screen name='Details' component={DetailsScreen} />
-          <Stack.Screen name='Search or Post' component={SearchPost} />
-          <Stack.Screen name='I want to' component={WantTo} />
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='My Account' component={MyAccount} />
-          <Stack.Screen name='SignInUp' component={SignInUp}
+        <Stack.Navigator
+          screenOptions={{
+            cardStyle: {
+              backgroundColor: "white",
+            },
+          }}
+          initialRouteName="Home"
+        >
+          <Stack.Screen name="Brand" component={Brand} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Search or Post" component={SearchPost} />
+          <Stack.Screen name="I want to" component={WantTo} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="My Account" component={MyAccount} />
+          <Stack.Screen name="About Me" component={AboutMe} />
+          <Stack.Screen
+            name="SignInUp"
+            component={SignInUp}
             options={{
               headerTransparent: true,
             }}
@@ -97,10 +115,10 @@ function App() {
 // Style Part
 const styles = StyleSheet.create({
   titleText: {
-    fontFamily: 'Lato-Black',
+    fontFamily: "Lato-Black",
     fontSize: 100,
     // fontWeight: 'bold',
-    color: '#1ed9a6',
+    color: "#1ed9a6",
   },
 });
 
